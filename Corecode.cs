09d54ef2bb8b0eb3829f99b46d338eb823131e29
Vanjatop228 <125455102+Vanjatop228@users.noreply.github.com>
2023-02-15 14:42:28 +0200

    Update Corecode.cs

--- Corecode.cs
+++ Corecode.cs
@@ -1,2 +1,14 @@
 int[] cars {10}
 Console.WriteLine(cars)
+cars[0] = 14;
+cars[1] = 4;
+cars[2] = 9;
+cars[3] = 7;
+cars[4] = 0
+int[] cars {1000}
+Random.rand = Random();
+for (i=0, i <= cars, i++)
+{
+cars[i] = rand.Next(0, 100000000)
+Console.WriteLine(cars[i])
+}
