e521bc6808c0f8f19a47352c3ab80c20259e5a50
Vanjatop228 <125455102+Vanjatop228@users.noreply.github.com>
2023-02-15 14:45:27 +0200

    Update Corecode.cs

--- Corecode.cs
+++ Corecode.cs
@@ -1,11 +1,11 @@
-int[] cars {10}
+int[] cars = {100000}
 Console.WriteLine(cars)
 cars[0] = 14;
 cars[1] = 4;
 cars[2] = 9;
 cars[3] = 7;
 cars[4] = 0
-int[] cars {1000}
+
 Random.rand = Random();
 for (i=0, i <= cars, i++)
 {
